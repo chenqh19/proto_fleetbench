004d1209fe6dce5540c23cb9746069b992661d98
chenqh19 <chenqh23@mit.edu>
2025-03-04 16:13:06 +0000

    debug performance

--- main.cc
+++ main.cc
@@ -3,19 +3,17 @@
 #include "google/protobuf/arena.h"
 
 int main() {
-    // Create arena for memory management
-    google::protobuf::Arena arena;
     
     // Create lifecycle object
     proto::ProtoLifecycle lifecycle(10);
     
-    // Initialize lifecycle
-    lifecycle.Init(&arena);
     
     // Run lifecycle 10 times
     std::cout << "Running lifecycle 10 times..." << std::endl;
     for (int i = 0; i < 10; i++) {
         std::cout << "Iteration " << (i + 1) << std::endl;
+        google::protobuf::Arena arena;
+        lifecycle.Init(&arena);
         lifecycle.Run();
     }
     
