1ef631fa6a9347d03f17811ae8f61fd163543e49
chenqh19 <chenqh23@mit.edu>
2025-03-10 12:20:19 -0700

    print more detailed stats

--- receiver.h
+++ receiver.h
@@ -32,8 +32,20 @@ Ser1de* ser1de;
 std::chrono::duration<double> total_serialize_time;
 std::chrono::duration<double> total_deserialize_time;
 
+std::chrono::duration<double> total_merge_time;
+std::chrono::duration<double> total_destroy_time;
+std::chrono::duration<double> total_copy_time;
+std::chrono::duration<double> total_clear_time;
+std::chrono::duration<double> total_create_time;
+
+size_t total_merge_count = 0;
+size_t total_destroy_count = 0;
+size_t total_copy_count = 0;
+size_t total_clear_count = 0;
+size_t total_create_count = 0;
+
 void InitializeSer1de() {
-  ser1de = new Ser1de("Software");
+  ser1de = new Ser1de();
 }
 
 size_t total_serialize_count = 0;
@@ -48,6 +60,27 @@ void DestroySer1de() {
   std::cout << "Total deserialize time: " << std::chrono::duration_cast<std::chrono::milliseconds>(total_deserialize_time).count() << "ms" << std::endl;
   std::cout << "Average deserialize time: " << std::chrono::duration_cast<std::chrono::microseconds>(total_deserialize_time / total_deserialize_count).count() << "us" << std::endl;
   std::cout << "Total deserialize count: " << total_deserialize_count << std::endl;
+  
+  std::cout << "Total merge time: " << std::chrono::duration_cast<std::chrono::milliseconds>(total_merge_time).count() << "ms" << std::endl;
+  std::cout << "Average merge time: " << std::chrono::duration_cast<std::chrono::microseconds>(total_merge_time / total_merge_count).count() << "us" << std::endl;
+  std::cout << "Total merge count: " << total_merge_count << std::endl;
+  
+  std::cout << "Total destroy time: " << std::chrono::duration_cast<std::chrono::milliseconds>(total_destroy_time).count() << "ms" << std::endl;
+  std::cout << "Average destroy time: " << std::chrono::duration_cast<std::chrono::microseconds>(total_destroy_time / total_destroy_count).count() << "us" << std::endl;
+  std::cout << "Total destroy count: " << total_destroy_count << std::endl;
+  
+  std::cout << "Total copy time: " << std::chrono::duration_cast<std::chrono::milliseconds>(total_copy_time).count() << "ms" << std::endl;
+  std::cout << "Average copy time: " << std::chrono::duration_cast<std::chrono::microseconds>(total_copy_time / total_copy_count).count() << "us" << std::endl;
+  std::cout << "Total copy count: " << total_copy_count << std::endl;
+  
+  std::cout << "Total clear time: " << std::chrono::duration_cast<std::chrono::milliseconds>(total_clear_time).count() << "ms" << std::endl;
+  std::cout << "Average clear time: " << std::chrono::duration_cast<std::chrono::microseconds>(total_clear_time / total_clear_count).count() << "us" << std::endl;
+  std::cout << "Total clear count: " << total_clear_count << std::endl;
+  
+  std::cout << "Total create time: " << std::chrono::duration_cast<std::chrono::milliseconds>(total_create_time).count() << "ms" << std::endl;
+  std::cout << "Average create time: " << std::chrono::duration_cast<std::chrono::microseconds>(total_create_time / total_create_count).count() << "us" << std::endl;
+  std::cout << "Total create count: " << total_create_count << std::endl;
+  
   std::cout << "----------------------------------------" << std::endl;
 }
 
@@ -90,22 +123,34 @@ inline void ReceiveUint64(const uint64_t val) { Receive(val); }
 
 template <typename M>
 void Copy(M* message, M* other_message) {
+  auto start = std::chrono::high_resolution_clock::now();
   message->CopyFrom(*other_message);
-  // benchmark::ClobberMemory();
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+  total_copy_time += duration;
+  total_copy_count++;
   std::atomic_signal_fence(std::memory_order_acq_rel);
 }
 
 template <typename M>
 void Clear(M* message) {
+  auto start = std::chrono::high_resolution_clock::now();
   message->Clear();
-  // benchmark::ClobberMemory();
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+  total_clear_time += duration;
+  total_clear_count++;
   std::atomic_signal_fence(std::memory_order_acq_rel);
 }
 
 template <typename M>
 void Create(M* message) {
+  auto start = std::chrono::high_resolution_clock::now();
   *message = M();
-  // benchmark::ClobberMemory();
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+  total_create_time += duration;
+  total_create_count++;
   std::atomic_signal_fence(std::memory_order_acq_rel);
 }
 
@@ -113,10 +158,10 @@ template <typename M>
 void Deserialize(M* message, std::string* serialized) {
   auto start = std::chrono::high_resolution_clock::now();
   // ser1de->ParseFromStringDebug(*serialized, message);
-  ser1de->ParseFromString(*serialized, message);
-  // auto result = message->ParseFromString(*serialized);
+  // ser1de->ParseFromString(*serialized, message);
+  auto result = message->ParseFromString(*serialized);
   auto end = std::chrono::high_resolution_clock::now();
-  // asm volatile("" : : "r,m"(result) : "memory");
+  asm volatile("" : : "r,m"(result) : "memory");
   std::chrono::duration<double> duration = end - start;
   total_deserialize_time += duration;
   total_deserialize_count++;
@@ -124,8 +169,12 @@ void Deserialize(M* message, std::string* serialized) {
 
 template <typename M>
 void Destroy(M* message) {
+  auto start = std::chrono::high_resolution_clock::now();
   if (message->GetArena() == nullptr) delete message;
-  // benchmark::ClobberMemory();
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+  total_destroy_time += duration;
+  total_destroy_count++;
   std::atomic_signal_fence(std::memory_order_acq_rel);
 }
 
@@ -149,8 +198,12 @@ void IsInitialized(M* message) {
 
 template <typename M>
 void Merge(M* message, M* other_message) {
+  auto start = std::chrono::high_resolution_clock::now();
   message->MergeFrom(*other_message);
-  // benchmark::ClobberMemory();
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> duration = end - start;
+  total_merge_time += duration;
+  total_merge_count++;
   std::atomic_signal_fence(std::memory_order_acq_rel);
 }
 
@@ -158,8 +211,8 @@ template <typename M>
 void Serialize(M* message, std::string* serialized) {
   auto start = std::chrono::high_resolution_clock::now();
   // ser1de->SerializeToStringDebug(*message, serialized);
-  ser1de->SerializeToString(*message, serialized);
-  // message->SerializeToString(serialized);
+  // ser1de->SerializeToString(*message, serialized);
+  message->SerializeToString(serialized);
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> duration = end - start;
   total_serialize_time += duration;
